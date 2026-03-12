ca96e292e44bfaae8f30262209d22c437a4ca4e9
Trevor Basinger <trevor.basinger@gmail.com>
2026-03-12 19:30:21 +0000

    Remove deleted Ray wrapper files

--- roar/ray/fragment_key.py
+++
@@ -1,8 +0,0 @@
-from roar.services.execution import fragment_sessions
-
-key_file_path = fragment_sessions.fragment_session_path
-generate_fragment_key = fragment_sessions.generate_fragment_session
-load_key = fragment_sessions.load_fragment_session
-save_key = fragment_sessions.save_fragment_session
-
-__all__ = ["generate_fragment_key", "key_file_path", "load_key", "save_key"]
--- roar/ray/glaas_fragment_streamer.py
+++
@@ -1,3 +0,0 @@
-from roar.glaas.fragment_streamer import GlaasFragmentStreamer
-
-__all__ = ["GlaasFragmentStreamer"]
